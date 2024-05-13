4022e48af1d814e1d2007edb3a7c960d320f1f76
mustapha <moustaphaoussama1818@gmail.com>
2024-05-13 16:11:36 +0100

    create function that display a dialog to update info of user

--- userProfile.js
+++ userProfile.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getFirestore, doc, getDoc, getDocs, collection  } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { getFirestore, doc, getDoc, getDocs, collection, updateDoc  } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 import { getAuth, onAuthStateChanged   } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+
 
 
 // Your web app's Firebase configuration
@@ -18,6 +19,8 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const myDatabase = getFirestore(app);
 
+let currentUser;
+let currentUserRef;
 
 onAuthStateChanged(auth, async (user) => {
     if (user) {
@@ -27,7 +30,8 @@ onAuthStateChanged(auth, async (user) => {
       const querySnapshot = await getDocs(collection(myDatabase, "clients"));
         querySnapshot.forEach((doc) => {
           if(currentUserUid === doc.data().uid ) {
-            const currentUser = doc.data();
+            currentUser = doc.data();
+            currentUserRef = doc.ref; 
             showClientInfo(currentUser);
           } 
         });
@@ -36,6 +40,18 @@ onAuthStateChanged(auth, async (user) => {
       // User is signed out
     }
   });
+
+
+
+
+const editBtn = document.querySelector('.editBtn');
+const dialog = document.querySelector('dialog');
+editBtn.addEventListener('click', () => {
+  dialog.innerHTML = '';
+  dialog.showModal();
+  //fill user info in our inputs
+  dialog.appendChild(createDialogForm(currentUser));
+});
 
 
 function showClientInfo(user) {
@@ -60,3 +76,69 @@ function showClientInfo(user) {
   infoSection.appendChild(phoneNumberDiv);
   
 }  
+//create form with the info of user inside dialog
+function createDialogForm(user) {
+  const form = document.createElement('form');
+
+  const adresseDiv = document.createElement('div');
+  adresseDiv.classList.add('adresse');
+    //create inputs ta3 province , city ..
+    const inputCity = document.createElement('input');
+    inputCity.value = user.city;
+    const inputProvince = document.createElement('input');
+    inputProvince.value = user.province;
+    const inputStreet = document.createElement('input');
+    inputStreet.value = user.street;
+
+    adresseDiv.appendChild(inputCity);
+    adresseDiv.appendChild(inputProvince);
+    adresseDiv.appendChild(inputStreet);
+  
+  const emailInput = document.createElement('input');  
+  emailInput.type = 'email';
+  emailInput.value = user.email;
+  const telInput = document.createElement('input');  
+  telInput.type = 'tel';
+  telInput.value = user.phoneNumber;
+
+  //div who contains the buttons
+  const btnDiv = document.createElement('div');
+  btnDiv.classList.add('btnDiv');
+    const submitBtn = document.createElement('button');
+    submitBtn.textContent = 'Ok';
+    //onclick 3la ok btn
+    submitBtn.addEventListener('click', (e)=> {
+      e.preventDefault();
+      const newData = {
+        email: emailInput.value,
+        phoneNumber: telInput.value.toString(),
+        city: inputCity.value,
+        province: inputProvince.value,
+        street: inputStreet.value 
+      }
+      //fonction ta3 firebase ..dir update ll info ta3 user bla matim7i
+      updateDoc(currentUserRef, newData);
+    })
+
+    const closeBtn = document.createElement('button');
+    closeBtn.textContent = 'Close';
+    closeBtn.addEventListener('click', (event) => {
+      event.preventDefault();
+      dialog.innerHTML = '';
+      dialog.close();
+    })
+
+    btnDiv.appendChild(submitBtn);  
+    btnDiv.appendChild(closeBtn);  
+    btnDiv.style.display = 'flex'; 
+
+  form.appendChild(adresseDiv) ;
+  form.appendChild(emailInput);  
+  form.appendChild(telInput);
+  form.appendChild(btnDiv);  
+
+
+
+  return form ;
+}
+
