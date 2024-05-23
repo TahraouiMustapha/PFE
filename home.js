d2c15b044ef6c740b31b7b8890c11c0c31d2bc0c
BelkacemRouibi <belkacemrouibi@outlook.com>
2024-05-23 04:38:27 +0200

    smhl
    w smhli w smhli

--- home.js
+++ home.js
@@ -8,19 +8,17 @@ const craftingImages = [
 
 let currentIndex = 0;
 
-// Fonction pour changer l'image toutes les 4 secondes
+// Fonction pour changer image toutes les 4 secondes
 function changeCraftingImage() {
   craftingImage.src = craftingImages[currentIndex];
   currentIndex = (currentIndex + 1) % craftingImages.length;
 }
-
-// Commencez le diaporama immédiatement
-changeCraftingImage();
-
+// changeCraftingImage();
 // Changez l'image toutes les 4 secondes
 setInterval(changeCraftingImage, 4000);
 // end crafting
 //  start populat service
+
 // var swiper = new Swiper(".services-main", {
 //   slidesPerView: 3,
 //   spaceBetween: 25,
@@ -57,33 +55,19 @@ setInterval(changeCraftingImage, 4000);
 //     dynamicBullets: true,
 //   },
 // });
-// Sélectionnez l'élément avec la classe "services-main"
-
 var swiper = new Swiper(".services-main", {
   slidesPerView: 3,
   spaceBetween: 25,
-  loop: false,
-  centeredSlides: false,
-  grabCursor: true,
+  loop: false, // Ajout de la boucle pour permettre un défilement continu
+  centeredSlides: true, // Utilisation de la centralisation des slides
+  fade: true, // J'ai supposé que vous vouliez utiliser le fondu, sinon, vous pouvez le retirer
   slidesPerGroupSkip: 1,
   keyboard: {
     enabled: true,
   },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    950: {
-      slidesPerView: 3,
-    },
-  },
+
   scrollbar: {
     el: ".swiper-scrollbar",
-    hide: false,
-    draggable: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
@@ -94,61 +78,8 @@ var swiper = new Swiper(".services-main", {
     clickable: true,
     dynamicBullets: true,
   },
-  on: {
-    reachEnd: function () {
-      document.querySelector(".swiper-button-next").style.display = "hidden";
-      document.querySelector(".swiper-scrollbar").style.display = "none";
-      document.querySelector(".swiper-button-prev").style.display = "block";
-    },
-    reachBeginning: function () {
-      document.querySelector(".swiper-button-prev").style.display = "none";
-      document.querySelector(".swiper-scrollbar").style.display = "none";
-      document.querySelector(".swiper-button-next").style.display = "block";
-    },
-    fromEdge: function () {
-      document.querySelector(".swiper-button-next").style.display = "block";
-      document.querySelector(".swiper-button-prev").style.display = "block";
-      document.querySelector(".swiper-scrollbar").style.display = "block";
-    },
-    slideChange: function () {
-      updateSwiperButtons(swiper);
-    },
-  },
 });
 
-// Function to update the navigation buttons and scrollbar state
-function updateSwiperButtons(swiper) {
-  if (swiper.isBeginning) {
-    // document.querySelector(".swiper-button-prev").style.display = "none";
-    // document.querySelector(".swiper-scrollbar").style.display = "none";
-  } else if (swiper.isEnd) {
-    document.querySelector(".swiper-button-next").style.display = "none";
-    document.querySelector(".swiper-scrollbar").style.display = "none";
-  } else {
-    // document.querySelector(".swiper-button-next").style.display = "block";
-    // document.querySelector(".swiper-button-prev").style.display = "block";
-    // document.querySelector(".swiper-scrollbar").style.display = "block";
-  }
-}
-
-// Initialize Swiper and update buttons on init
-swiper.on("init", function () {
-  updateSwiperButtons(swiper);
-});
-
-// Initialize Swiper
-swiper.init();
-swiper.update(); // Ensure Swiper is updated to reflect initial state
-
-// Add a manual check in case the initial state is not correctly set
-if (swiper.isEnd) {
-  document.querySelector(".swiper-button-next").style.display = "none";
-  document.querySelector(".swiper-scrollbar").style.display = "none";
-}
-if (swiper.isBeginning) {
-  // document.querySelector(".swiper-button-prev").style.display = "none";
-  // document.querySelector(".swiper-scrollbar").style.display = "none";
-}
 // end popular service
 // start categorie
 // function addCategory() {
@@ -172,3 +103,30 @@ if (swiper.isBeginning) {
 // }
 
 // end categorie
+// start best sellers
+
+//  end sellers
+// start tips
+
+const tipsImage = document.querySelector(".tips .tips-photo");
+const tipsImages = [
+  "./images/1tips.jpg",
+  "./images/2tips.png",
+  "./images/3tips.jpg",
+  "./images/5tips.jpg",
+  "./images/6tips.png",
+  "./images/7tips.jpg",
+  "./images/8tips.webp",
+  "./images/9tips.jpg",
+  "./images/10tips.jpeg",
+  "./images/11tips.jpg",
+];
+let tipsIndex = 0;
+
+function changeTipsImage() {
+  tipsImage.src = tipsImages[tipsIndex];
+  tipsIndex = (tipsIndex + 1) % tipsImages.length;
+}
+
+// Changer l'image toutes les 4 secondes
+setInterval(changeTipsImage, 2000);
