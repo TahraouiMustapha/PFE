08e32e99f3178d4b87f81eb06a0b68a9fd2c1eca
mustapha <moustaphaoussama1818@gmail.com>
2024-06-03 22:15:10 +0100

    adding condition if is worker

--- workerProfile.js
+++ workerProfile.js
@@ -1,3 +1,51 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import {
+  getFirestore,
+  getDocs,
+  collection,
+} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+//import from our searchModule.js
+import { onkeyUpHandler, getArrayCategory } from "./searchModule.js";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOa527QiPGbAmOXtbG99sBoKmwGpTrE6k",
+  authDomain: "projet-fin-d-etude-975b7.firebaseapp.com",
+  databaseURL: "https://projet-fin-d-etude-975b7-default-rtdb.firebaseio.com",
+  projectId: "projet-fin-d-etude-975b7",
+  storageBucket: "projet-fin-d-etude-975b7.appspot.com",
+  messagingSenderId: "919112985393",
+  appId: "1:919112985393:web:7d9c3545b45b52584dde10",
+};
+
+const app = initializeApp(firebaseConfig);
+const myDatabase = getFirestore(app);
+const auth = getAuth();
+
+let isWorker = false;
+
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    // User is signed in
+    const currentUserUid = user.uid;
+
+    if(checkClient(currentUserUid)) {
+      console.log('client');
+    } else if(checkWorker(currentUserUid)) {
+      isWorker = true;
+    }
+
+  } else {
+    // User is signed out
+  }
+});
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(window.location.search);
   const workerJson = urlParams.get("worker");
@@ -14,11 +62,37 @@ document.addEventListener("DOMContentLoaded", () => {
   mainSec.appendChild(createServiceTitle(worker.speciality));
   mainSec.appendChild(createServiceContent(worker));
   mainSec.appendChild(createServiceCommande(worker));
-  ifThereNew(worker.hasNew);
-
-
+  if(isWorker) {
+    ifThereNew(worker.hasNew);
+  }
 
 });
+
+
+
+//funciton if client
+async function checkClient(userUId) {
+  const querySnapshot = await getDocs(collection(myDatabase, "clients"));
+  querySnapshot.forEach((doc) => {
+    if (userUId === doc.data().uid) {
+      let currentUser = doc.data();
+      let currentUserRef = doc.ref;
+      return true;
+    }
+  });
+}
+
+//function if worker
+async function checkWorker(userUId) {
+  const querySnapshot = await getDocs(collection(myDatabase, "workers"));
+  querySnapshot.forEach((doc) => {
+    if (userUId === doc.data().uid) {
+      let currentUser = doc.data();
+      let currentUserRef = doc.ref;
+      return true;
+    }
+  });
+}
 
 //func to create service-title div
 function createServiceTitle(spec) {
@@ -295,6 +369,7 @@ function one() {
     divvisble.style.display = "block";
   }
 }
+
 notification.addEventListener('click', () => {
   one();
 })
